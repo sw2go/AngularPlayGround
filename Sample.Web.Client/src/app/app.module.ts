7218bcb7f1f71d076634c6966461dec9c0377080
aa <Andre.Achermann1@swisscom.com>
2018-04-27 16:13:57 +0200

    edit

--- Sample.Web.Client/src/app/app.module.ts
+++ Sample.Web.Client/src/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {NgbdCarouselBasic} from './carousel-basic/carousel-basic.component';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbdCarouselBasic
   ],
   providers: [],
   bootstrap: [AppComponent]
